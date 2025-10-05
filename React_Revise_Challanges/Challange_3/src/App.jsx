ff0cb71eed6206d452f925cecb31566e316c0c7b
Akash <akashj.vasava@gmail.com>
2025-10-05 20:27:58 +0530

    Completed Third Challange of the Series

---
+++ React_Revise_Challanges/Challange_3/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Greetings from './components/Greetings'
+import ProductInfo from './components/ProductInfo'
+
+const App = () => {
+  return (
+    <div>
+      <Greetings />
+      <ProductInfo />
+    </div>
+  )
+}
+
+export default App
