1d51d68437626e198cfc7dc9dbd4559fa34a4229
Akash <akashj.vasava@gmail.com>
2025-10-23 14:20:40 +0530

    Learned about the concepts of Framer Motion

--- Framer_Motion/Learning-1/src/App.tsx
+++ Framer_Motion/Learning-1/src/App.tsx
@@ -1,7 +1,18 @@
+import { motion } from "motion/react"
 
 const App = () => {
   return (
-    <div className='text-3xl'>App</div>
+    <div>
+      {/* Simple Animaiton Properties */}
+      <motion.div className="box" animate={{x:20}} />
+      <motion.div className="square" animate={{rotate: 20}} />
+      <motion.div className="square" animate={{scale: 1.2}} />
+      <motion.div className="square" animate={{skewX: 20}} />
+
+      {/* Transitions in Framer Motion */}
+      <motion.div className="box" initial={{x:0}} animate={{x: 300}} transition={{duration: 5, ease:"backIn"}} />
+      <motion.div className="box" animate={{scale: [1,2,4,2,1]}} transition={{duration: 4}} />
+    </div>
   )
 }
 
---
+++ Framer_Motion/Learning-2/src/App.tsx
@@ -0,0 +1,11 @@
+import PulsingButton from "./Components/PulsingButton"
+
+const App = () => {
+  return (
+    <div>
+      <PulsingButton />
+    </div>
+  )
+}
+
+export default App
